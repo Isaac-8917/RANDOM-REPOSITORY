48893ac4f5a79c7f6349ddb9b7c8a31bad347cd3
Isaac_#8917 <103591767+Isaac-8917@users.noreply.github.com>
2023-03-21 11:16:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ userName = input("Hello user! What is your name? (Dont put a space)")
 userConfirmation = print(f"Hello, Private {userName}!")
 
 print("Welcome to the Army Academy!")
+print("Space then Enter to Continue")
+Continue = input("> ")
+if(Continue == " "):
+  print("Your goal is to climb the ranks of the Army.")
