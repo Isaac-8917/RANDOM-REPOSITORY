58f98e4a37a36927f86e55987a21a49ab546aa6e
Isaac_#8917 <103591767+Isaac-8917@users.noreply.github.com>
2023-04-11 10:22:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-#the project has been scrapped for the time being because of a lot of bugs so i decided to start over. I will likely start a similar project very soon.
+#the project has been scrapped for the time being because of a huge mistake that would be very difficult to fix to the point where it is more worth it 
+#to start over.
+#I will likely start a similar project very soon.
